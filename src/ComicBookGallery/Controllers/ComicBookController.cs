055ed50b6d8e21b7ba092cf9e9abaf4c7bbb5c65
Quinn Zepeda <=>
2017-07-24 10:03:26 -0700

    Updated Detail action method.

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -8,9 +8,17 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBookController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the comic book controller.";
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
+            {
+                return Redirect("/");
+            };
+
+            return Content("Hello from the comic book controller.");
+          
+
+
         }
     }
 }
